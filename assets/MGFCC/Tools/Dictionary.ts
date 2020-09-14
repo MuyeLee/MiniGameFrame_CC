dd7d43dbce0344575e022b024fd1994e980eb444
李小布 <397554852@qq.com>
2020-09-14 11:01:58 +0800

    字典增加json方法

--- assets/MGFCC/Tools/Dictionary.ts
+++ assets/MGFCC/Tools/Dictionary.ts
@@ -1,17 +1,17 @@
 
 export default class Dictionary<K, V>{
-    private keys:Array<K> = [];
-    private values:Array<V> = [];
+    private keys: Array<K> = [];
+    private values: Array<V> = [];
 
     /**
      * 存储数据
      * @param key 数据key
      * @param value 数据
      */
-    public put(key:K,value:V){
+    public put(key: K, value: V) {
         let index = 0;
-        while(index < this.keys.length){
-            if(this.keys[index] == key){
+        while (index < this.keys.length) {
+            if (this.keys[index] == key) {
                 this.keys[index] = key;
                 this.values[index] = value;
                 return true;
@@ -27,10 +27,10 @@ export default class Dictionary<K, V>{
      * 获取数据
      * @param key 数据key
      */
-    public get(key:K){
+    public get(key: K) {
         let index = 0;
-        while(index < this.keys.length){
-            if(this.keys[index] == key){
+        while (index < this.keys.length) {
+            if (this.keys[index] == key) {
                 return this.values[index];
             }
             index++;
@@ -42,12 +42,12 @@ export default class Dictionary<K, V>{
      * 删除数据
      * @param key 数据key 
      */
-    public remove(key:K){
+    public remove(key: K) {
         let index = 0;
-        while(index < this.keys.length){
-            if(this.keys[index] == key){
-                this.keys.splice(index,1);
-                this.values.splice(index,1);
+        while (index < this.keys.length) {
+            if (this.keys[index] == key) {
+                this.keys.splice(index, 1);
+                this.values.splice(index, 1);
                 return true;
             }
             index++;
@@ -58,8 +58,8 @@ export default class Dictionary<K, V>{
     /**
      * 清空字典
      */
-    public clear(){
-        this.keys.splice(0,this.keys.length);
+    public clear() {
+        this.keys.splice(0, this.keys.length);
         this.values.splice(0, this.values.length);
     }
 
@@ -67,10 +67,10 @@ export default class Dictionary<K, V>{
      * 检测
      * @param key 数据key
      */
-    public containkey(key:K){
+    public containkey(key: K) {
         let index = 0;
-        while(index < this.keys.length){
-            if(this.keys[index] == key)return true;
+        while (index < this.keys.length) {
+            if (this.keys[index] == key) return true;
             index++;
         }
         return false;
@@ -79,18 +79,47 @@ export default class Dictionary<K, V>{
     /**
      * 获取全部数据
      */
-    public getValues(){
+    public getValues() {
         return this.values.slice(0);
     }
 
     /**
      * 获取全部key
      */
-    public getKeys(){
+    public getKeys() {
         return this.keys.slice(0);
     }
 
-    public getCount():number{
+    /**
+     * 获取字典长度
+     * @param return 字典长度
+     */
+    public getCount(): number {
         return this.keys.length;
+    }
+
+    /**
+     * 转换成json字符串
+     * @param return json字符串
+     */
+    public stringify(): string {
+        return JSON.stringify(this);
+    }
+
+    /**
+     * 从json字符串创建字典
+     * @param data json字符串
+     * @param return 字典
+     */
+    public fromJson(data: string) {
+        try {
+            let json = JSON.parse(data);
+            this.keys = json.keys;
+            this.values = json.values;
+            return this;
+        } catch {
+            console.error("转换错误！");
+            return null;
+        }
     }
 }
